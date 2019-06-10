50d322a5d33916b8704d7bc11be38be43ec30750
unio <unioblog@gmail.com>
2019-06-10 17:38:37 +0900

    Updates

---
+++ precache-manifest.b7a6b8a7119ac19406c0ba0b5e749901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d3af5cd0c53e2109c76a9e59781db9b",
+    "url": "/mannabin/index.html"
+  },
+  {
+    "revision": "bf4322ea484f2fa55c70",
+    "url": "/mannabin/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e0c9bdaae556aad6849b",
+    "url": "/mannabin/static/js/2.c27c9171.chunk.js"
+  },
+  {
+    "revision": "bf4322ea484f2fa55c70",
+    "url": "/mannabin/static/js/main.56acb91c.chunk.js"
+  },
+  {
+    "revision": "0ba98c9b331245c8b9a7",
+    "url": "/mannabin/static/js/runtime~main.8b722c9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mannabin/static/media/logo.5d5d9eef.svg"
+  }
+]);
